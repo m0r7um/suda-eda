c0121fab8066b3a3cf8eb8d91de527aa7a130310
martin.valts <martin.valtz@gmail.com>
2025-03-14 22:01:43 +0300

    project structure

---
+++ src/main/java/org/food/sudaeda/SudaEdaApplication.java
@@ -0,0 +1,13 @@
+package org.food.sudaeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudaEdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SudaEdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/food/sudaeda/SudaEdaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.food.sudaeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudaEdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

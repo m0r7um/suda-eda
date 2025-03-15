8c2ab4cca9eab3eb8549c4a431b0ac257d82b44a
martin.valts <martin.valtz@gmail.com>
2025-03-15 13:46:46 +0300

    user repo method

--- src/main/java/org/food/sudaeda/core/repository/UserRepository.java
+++ src/main/java/org/food/sudaeda/core/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package org.food.sudaeda.core.repository;
 
 import java.util.List;
+import java.util.Optional;
+import org.food.sudaeda.core.enums.Role;
 import org.food.sudaeda.core.model.User;
 import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +13,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT c FROM User c WHERE c.role = 'COURIER' AND NOT EXISTS(SELECT so FROM SuggestedOrder so WHERE so.courier.id <> c.id AND so.status != 'REJECTED')")
     List<User> findFreeCouriers(Limit limit);
+    Optional<User> findByIdAndRole(Long id, Role role);
 }

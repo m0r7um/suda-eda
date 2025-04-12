fd5573466674c20deb0911babcc3e683f731b1f6
maxbarsukov <maximbarsukov@bk.ru>
2025-04-12 12:04:42 +0300

    chore: :wrench: configure analytics db for liquibase

--- src/main/java/org/food/sudaeda/SudaEdaApplication.java
+++ src/main/java/org/food/sudaeda/SudaEdaApplication.java
@@ -2,12 +2,18 @@ package org.food.sudaeda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        LiquibaseAutoConfiguration.class
+})
 public class SudaEdaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SudaEdaApplication.class, args);
     }
-
 }

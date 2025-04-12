4c73cede32a665cfbf96adb58d6c67457f36185c
maxbarsukov <maximbarsukov@bk.ru>
2025-04-12 12:41:05 +0300

    feat: :sparkles: add order update service

---
+++ src/main/java/org/food/sudaeda/analytics/service/OrderUpdateService.java
@@ -0,0 +1,33 @@
+package org.food.sudaeda.analytics.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.food.sudaeda.analytics.model.OrderUpdate;
+import org.food.sudaeda.analytics.repository.OrderUpdateRepository;
+import org.food.sudaeda.core.enums.OrderStatus;
+import org.food.sudaeda.exception.NotFoundException;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderUpdateService {
+    private final OrderUpdateRepository orderUpdateRepository;
+
+    public OrderUpdate add(Long orderId, OrderStatus fromStatus, OrderStatus toStatus) {
+        OrderUpdate orderUpdate = OrderUpdate.builder()
+                .orderId(orderId)
+                .fromStatus(fromStatus)
+                .toStatus(toStatus)
+                .createdAt(LocalDateTime.now())
+                .build();
+        return orderUpdateRepository.save(orderUpdate);
+    }
+
+    public OrderUpdate getOrderUpdateById(Long id) {
+        return orderUpdateRepository.findById(id).orElseThrow(() -> new NotFoundException("Order update not found"));
+    }
+}

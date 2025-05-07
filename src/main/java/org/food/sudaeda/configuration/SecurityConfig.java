a89607df178aca60a7a1fa68471a55c937cb3c80
martin.valts <martin.valtz@gmail.com>
2025-05-07 17:42:56 +0300

    fixes

--- src/main/java/org/food/sudaeda/configuration/SecurityConfig.java
+++ src/main/java/org/food/sudaeda/configuration/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
